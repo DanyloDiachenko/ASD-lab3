d3cc0ed1093d442bb9dc8294634a187100e31be6
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-10-05 19:29:38 +0300

    code fixes

--- index.c
+++ index.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <math.h>
 
-double factorial(int n);
 double calculateExponential(double x, double epsilon);
 
 int main() {
-    const double x = 10;
-    const double epsilon = 1;
+    const double x = 1;
+    const double epsilon = 0.001;
 
     const double result = calculateExponential(x, epsilon);
     printf("%lf\n", result);
@@ -14,23 +13,13 @@ int main() {
     return 0;
 }
 
-double factorial(const int n) {
-    double result = 1.0;
-
-    for (int i = 1; i <= n; i++) {
-        result *= i;
-    }
-
-    return result;
-}
-
 double calculateExponential(const double x, const double epsilon) {
-    double sum = 1.0;  // Початковий елемент суми
-    double term = 1.0; // Термін ряду (член)
+    double sum = 1.0;
+    double term = 1.0;
     int n = 1;
 
     while (fabs(term) > epsilon) {
-        term = pow(-1, n) * pow(x * x, n) / factorial(n); // Обчислення наступного члена
+        term *= x / n;
         sum += term;
         n++;
     }
