ff976fbf0041e68b7077a7f914d3a30fe7c4c65f
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-10-12 17:31:09 +0300

    FIXES

--- index.c
+++ index.c
@@ -1,28 +1,41 @@
 #include <stdio.h>
 #include <math.h>
 
+double factorial(int n);
+
 double calculateExponential(double x, double epsilon);
 
 int main() {
-    const double x = 1;
-    const double epsilon = 0.001;
+    double x = 4;
+    double epsilon = 0.001;
 
-    const double result = calculateExponential(x, epsilon);
-    printf("%lf\n", result);
+    double result = calculateExponential(x, epsilon);
+
+    printf("%f\n", result);
 
     return 0;
 }
 
-double calculateExponential(const double x, const double epsilon) {
-    double sum = 1.0;
+double calculateExponential(double x, double epsilon) {
     double term = 1.0;
+    double sum = 1.0;
     int n = 1;
 
     while (fabs(term) > epsilon) {
-        term *= x / n;
+        term = pow(-1, n) * pow(x, 2 * n) / factorial(n);
         sum += term;
         n++;
     }
 
     return sum;
+}
+
+double factorial(int n) {
+    double factorial = 1.0;
+
+    for (int i = 2; i <= n; i++) {
+        factorial *= i;
+    }
+
+    return factorial;
 }
